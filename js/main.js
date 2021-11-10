5008066ccef1665b81837c96b294a253e212f5d0
nghiemdoan <dthnghiem2111@gmail.com>
2021-11-10 16:52:09 -0600

    Update all files to Github, Clean up unused files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
+    //Filter Portfolio Grid
     let $btns = $('.project-area .button-group button');
     $btns.click(function(e){
-        debugger;
         $('.project-area .button-group button').removeClass('active');
         e.target.classList.add('active');
         let selector = $(e.target).attr('data-filter');
@@ -12,6 +12,41 @@ $(document).ready(function(){
     })
 
     $('.project-area .button-group #btn1').trigger('click');
+    
+    //Magnific Popup
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery:{enabled:true}
+        // other options
+    });
 
+    //Owl-carousel
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        dots:true,
+        responsive:{
+            0:{items:1},
+            544:{items:2}
+        }
+    })
+
+    //Sticky Navigation Menu
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navBarFixed(){
+        if($('.header_area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if(scroll >= nav_offset_top){
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navBarFixed();
 
 })

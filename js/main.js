bafa6953326f876b6c6f2304de3ff60fead8e37b
nghiemdoan <dthnghiem2111@gmail.com>
2021-11-11 13:22:37 -0600

    update index.html and image's quality

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
 
     //Click on All button to reset the grid display
     $('.project-area .button-group #btn-all').trigger('click');
+    $('.project-area .button-group #btn-all').trigger('click');
     
     //Magnific Popup
     $('.project-area .grid .test-popup-link').magnificPopup({
